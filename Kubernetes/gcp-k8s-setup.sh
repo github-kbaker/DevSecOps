a698df481f769aa5f80dafd4d226819ab9cf13ab
Wilson Mar <wilsonmar+github@gmail.com>
2017-12-01 14:54:54 +0000

    Create gcp-k8s-setup.sh

---
+++ Kubernetes/gcp-k8s-setup.sh
@@ -0,0 +1,14 @@
+# This
+# Run within a Google Console this command:
+# bash <(curl -s https://raw.githubusercontent.com/wilsonmar/DevSecOps/master/gcp-k8s.sh)
+# Described in https://wilsonmar.github.io/gcp 
+# This contains files used by tutorial
+# https://google.qwiklabs.com/focuses/7013 = Managing Deployments Using Kubernetes Engine
+
+curl -O https://raw.githubusercontent.com/wilsonmar/DevSecOps/master/deployments/auth.yaml)
+cat deployments/auth.yaml
+
+augments
+https://github.com/googlecodelabs/orchestrate-with-kubernetes
+
+kelseyhightower/hello
