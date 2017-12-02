5c1b380f3bd35be2eb74ee85312fd53fae41d9b6
Wilson Mar <wilsonmar+github@gmail.com>
2017-12-02 10:17:03 +0000

    Update k8s-gcp-hello.sh

--- Kubernetes/k8s-gcp-hello.sh
+++ Kubernetes/k8s-gcp-hello.sh
@@ -1,4 +1,5 @@
 # This bash script uses Kubernetes to establish within Google cloud a multi-service sample application named hello.
+# This automates manual steps within the Orchestrating the Cloud with Kubernetes lab at https://google.qwiklabs.com/focuses/7012
 
 # In a Goolge Cloud Console run this script using this command:
 # bash <(curl -s https://raw.githubusercontent.com/wilsonmar/DevSecOps/master/Kubernetes/k8s-gcp-hello.sh)
