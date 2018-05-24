cf95705cfe7dadf6df9bc5def0f862a87ebb09f9
Wilson Mar <wilsonmar+github@gmail.com>
2018-05-23 23:54:17 -0600

    Update qwik-gcp-cloudml.sh

--- qwiklabs/qwik-gcp-cloudml.sh
+++ qwiklabs/qwik-gcp-cloudml.sh
@@ -142,3 +142,16 @@ EVAL_DATA=gs://$BUCKET_NAME/data/adult.test.csv
 JOB_NAME=census1
 OUTPUT_PATH=gs://$BUCKET_NAME/$JOB_NAME
 echo $OUTPUT_PATH
+
+gcloud ml-engine jobs submit training $JOB_NAME \
+--job-dir $OUTPUT_PATH \
+--runtime-version 1.4 \
+--module-name trainer.task \
+--package-path trainer/ \
+--region $REGION \
+-- \
+--train-files $TRAIN_DATA \
+--eval-files $EVAL_DATA \
+--train-steps 5000 \
+--verbosity DEBUG
+
