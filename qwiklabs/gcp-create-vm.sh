f4f4366f4b2c6a6397eaa4765b7778f979057666
Wilson Mar <wilsonmar+github@gmail.com>
2018-05-25 18:28:54 -0600

    Create gcp-create-vm.sh

---
+++ qwiklabs/gcp-create-vm.sh
@@ -0,0 +1,16 @@
+#!/bin/bash -e
+
+# This performs the commands in "Creating a Virtual Machine" (GSP001) at
+#    https://google-run.qwiklab.com/focuses/4?parent=catalog
+# which is part of quest ...
+
+# Instead of typing, copy this command to run in the console within the cloud:
+# sh -c "$(curl -fsSL https://raw.githubusercontent.com/wilsonmar/DevSecOps/master/qwiklabs/gcp-create-vm.sh)"
+# by Wilson Mar, Wisdom Hambolu, and others.
+
+# This adds steps to grep values into variables for verification,
+# so you can spend time learning rather than typing and fixing typos.
+# This script deletes folders left over from previous run so can be rerun.
+# SCRIPT STATUS: WORKING. Results obtained after running twice on May 25, 2018.
+
+
