0982b5ef9522b0841e0c3298ca59879678789fae
Wilson Mar <wilsonmar+github@gmail.com>
2018-05-27 08:19:53 -0600

    Create terraform.sh

---
+++ aws/terraform.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+# https://linuxacademy.com/cp/livelabs/view/id/488
+
+python --version
+   # Python 2.7.12
+sudo apt-get update
+echo Y | sudo apt-get install git
+git clone https://github.com/linuxacademy/terransible
+cd terransible
+cd lab_scripts
+
+
+apt install python-pip
+
