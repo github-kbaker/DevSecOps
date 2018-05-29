7a3bcaf70a4557b1be3ee59087375cb974b70eb8
Wilson Mar <wilsonmar+github@gmail.com>
2018-05-28 21:54:44 -0600

    Update ec2-list.py

--- aws/ec2-list.py
+++ aws/ec2-list.py
@@ -1,6 +1,13 @@
 #!/usr/bin/env python
-# List EC2 instances.
+
 # ec2-list.py in https://github.com/wilsonmar/DevSecOps/new/master/aws
+# SCRIPT STATUS: UNDER CONSTRUCTION.
+# This List EC2 instances described in the hands-on blog at
+#    https://cloudacademy.com/blog/boto-python-automate-aws-services/ 
+
+# Instead of typing, copy this command to run in the console within the cloud:
+# sh -c "$(curl -fsSL https://raw.githubusercontent.com/wilsonmar/DevSecOps/master/aws/ec2-list.py)"
+
 import boto3
 ec2 = boto3.resource('ec2')
 for instance in ec2.instances.all():
