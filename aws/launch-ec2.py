5fb1b5e2b86c384fd0ce746620076d112f4c3bc8
Wilson Mar <wilsonmar+github@gmail.com>
2018-05-28 21:53:09 -0600

    Update launch-ec2.py

--- aws/launch-ec2.py
+++ aws/launch-ec2.py
@@ -1,5 +1,11 @@
 #/usr/bin/python
-# From https://cloudacademy.com/blog/boto-python-automate-aws-services/ 
+# SCRIPT STATUS: UNDER CONSTRUCTION.
+# This performs the commands described in the hands-on blog at
+#    https://cloudacademy.com/blog/boto-python-automate-aws-services/ 
+
+# Instead of typing, copy this command to run in the console within the cloud:
+# sh -c "$(curl -fsSL https://raw.githubusercontent.com/wilsonmar/DevSecOps/master/aws/launch-ec2.py)"
+
 import boto.ec2
 conn = boto.ec2.connect_to_region("us-west-2")
 conn.run_instances(
