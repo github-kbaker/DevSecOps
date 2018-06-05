58dd2e5769b2b660e77145fb9913ff966d5b6494
Kween Baker <31314351+github-kbaker@users.noreply.github.com>
2018-06-05 14:24:21 -0500

    Update hygieia.sh

--- aws/hygieia.sh
+++ aws/hygieia.sh
@@ -14,3 +14,12 @@ echo "yum install java-1.8.0-openjdk"
 sudo yum install java-1.7.0-openjdk-devel
 java -version
 export PATH=/usr/local/testing/jdk1.6.0_23/bin:$PATH
+
+# This is for downloading and installing apache maven for centos
+wget http://www.eu.apache.org/dist/maven/maven-3/3.3.9/binaries/apache-maven-3.3.9-bin.tar.gz
+tar xzf apache-maven-3.3.9-bin.tar.gz
+mkdir /usr/local/maven
+mv apache-maven-3.3.9/ /usr/local/maven/
+alternatives --install /usr/bin/mvn mvn /usr/local/maven/apache-maven-3.3.9/bin/mvn 1
+alternatives --config mvn
+
