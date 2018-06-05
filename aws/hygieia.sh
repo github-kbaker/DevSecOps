87e684887e181cecd578abbe0a49c8d3e57b4862
Kween Baker <31314351+github-kbaker@users.noreply.github.com>
2018-06-05 13:53:02 -0500

    Update hygieia.sh

--- aws/hygieia.sh
+++ aws/hygieia.sh
@@ -4,7 +4,9 @@
 # sh -c "$(curl -fsSL https://github.com/github-kbaker/DevSecOps/blob/master/aws/hygieia.sh)"
 # Following https://capitalone.github.io/Hygieia/setup.html
 
-git clone https://github.com/capitalone/Hygieia.git
+# git clone https://github.com/capitalone/Hygieia.git
 
             # https://capitalone.github.io/Hygieia/troubleshoot.html
             # https://github.com/bbyars/hygieia/issues/167#issuecomment-385420564
+            
+yum install java-1.8.0-openjdk            
