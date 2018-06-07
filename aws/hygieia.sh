ad0fcc65a9456152f499931689975536f67177fb
Kween Baker <31314351+github-kbaker@users.noreply.github.com>
2018-06-07 16:27:57 -0500

    Update hygieia.sh

--- aws/hygieia.sh
+++ aws/hygieia.sh
@@ -23,6 +23,72 @@ sudo ln -s /opt/apache-maven-3.3.9 /opt/apache-maven
 echo 'export PATH=$PATH:/opt/apache-maven/bin' | sudo tee -a /etc/profile
 source /etc/profile
 
+# Get the NODEJS here https://nodejs.org/en/download/package-manager/
+# NPM (Node package manager)
+# Run `npm i npm@latest -g` to upgrade your npm version
+curl --silent --location https://rpm.nodesource.com/setup_8.x | sudo bash -
+sudo yum -y install nodejs
+sudo yum install gcc-c++ make
+
+# Verify NODEJS and NPM installed
+node -v
+npm -v
+
+# Replace deprecated packages (i.e. gulp with gulp-webpack, etc) and rerun 'mvn clean..'
+npm i gulp-webpack
+npm i pug
+npm i minimatch
+npm i graceful-fs
+npm i tough-cookie
+npm i gulp-util
+npm i gulp-header
+npm i connect
+
+# Troubleshooting 'mvn clean install package' I have noticed frequently the below errors:
+#   “com.capitalona.dashboard:UI  Failure” 
+# -        Bower install (keyword in the error message)
+# -        npm install (keyword in the error message)
+# -        npm build (or) ./lib/engine.io (keyword in the error message)
+# https://github.com/capitalone/Hygieia/issues/1817
+# Troubleshooting suggests removing delete node_modules, dist and bower_components folders for resolving the UI error
+
+# Resolutions: re-install 'Bower and NPM' in ../UI/ directory the rerun ' mvn clean install package'
+
+# GOTO hygieia directory, run git clone and then 'mvn clean install package'
+cd /opt/hygieia
+git clone https://github.com/capitalone/hygieia
+mvn clean install package   # re-runnable and very useful in troubleshooting
+
+
+
+
+
+# Download and install mongo db from the below url based on the OS flavour you used.
+https://www.mongodb.com/download-center#previous
+
+# Create a mongodb-org-3.4.repo file  under /etc/yum.repos.d/  and copy/paste the below lines in the 
+# file(mongodb-org-3.4.repo). It is latest Database
+[mongodb-org-3.4]
+name=MongoDB Repository
+baseurl=https://repo.mongodb.org/yum/amazon/2013.03/mongodb-org/3.4/x86_64/
+gpgcheck=1
+enabled=1
+gpgkey=https://www.mongodb.org/static/pgp/server-3.4.asc
+
+# Install mongodb
+yum install mongodb-org
+
+# Validate mongodb status
+service mongodb status/start/restart/stop
+
+
+
+
+
+
+
+
+
 OLD PATH (redeveloping)
 # Referencing https://capitalone.github.io/Hygieia/setup.html, this routine will clone Hygieia to local EC2 instance
 # git clone https://github.com/capitalone/Hygieia.git
@@ -49,6 +115,8 @@ curl --silent --location https://rpm.nodesource.com/setup_8.x | sudo bash -
 sudo yum -y install nodejs
 
 # NPM (Node package manager)
+# Run `npm i npm@latest -g` to upgrade your npm version
+ 
 
 # GULP Install
 npm install gulp
