812809408e1bab3da3ccf68711a27c3272b857d5
Kween Baker <31314351+github-kbaker@users.noreply.github.com>
2018-06-07 08:26:30 -0500

    Update hygieia.sh

--- aws/hygieia.sh
+++ aws/hygieia.sh
@@ -34,3 +34,6 @@ alternatives --config mvn
 
 # This routine will run Maven Build In the command line/terminal, run the following command from the \Hygieia directory
 mvn clean install package
+
+# This routine start up API.JAR via java
+/home/jbaker/Hygieia/api/target]# java -jar api.jar --spring.config.name=api --spring.config.name=dashboard.properties --spring.config.location=/opt/git/Hygieia/api/dashboard.properties
